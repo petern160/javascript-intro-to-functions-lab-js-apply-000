ccca720bc6c8a8541782c9712ae9aa31558c1bce
Peter Nguyen <peternguyen160@gmail.com>
2018-12-04 08:19:30 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(input){
+  return input.toUpperCase();
+}
+
+function whisper(input){
+  return input.toLowerCase();
+}
+
+function logShout(input){
+  console.log(input.toUpperCase());
+}
